cd38af3b471b2cf740a214946944865bf9c58447
Dennis Kimtai <kimtai.developer@gmail.com>
2024-07-24 19:34:05 +0300

    add check for invalid token on verifyJWT middleware

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -1,7 +1,26 @@
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
+const usersDB = {
+    users: require('../models/users.json'),
+}
+
 const verifyJWT = (req,res,next) => {
+
+    const cookies = req.cookies;
+    
+    // Bad Request
+    if( !cookies?.jwt ) return res.sendStatus(401); // No content
+    const refreshToken = cookies.jwt;
+
+    const foundUser = usersDB.users.find( el => el.token === refreshToken)
+    
+    // Conflict username
+    if(!foundUser) {
+        res.clearCookie('jwt',{ httpOnly: true }) //sameSite: 'None', secure: true
+        return res.sendStatus(401)
+    }
+
     const authHeader = req.headers['authorization']
     if(!authHeader) return res.sendStatus(401)
     
